0706d8f320c1b9950a994bfeed86385019c39a28
hongchii <flyh0321@gmail.com>
2023-06-12 17:01:09 +0900

    jpashop

---
+++ jpashop/jpashop/src/main/java/jpabook/jspshop/domain/Member.java
@@ -0,0 +1,49 @@
+package jpabook.jspshop.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+    private String name;
+    private String street;
+    private String zipcode;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+}
---
+++ jpashop/jpashop/src/main/java/jpabook/jspshop/domain/OrderStatus.java
@@ -0,0 +1,5 @@
+package jpabook.jspshop.domain;
+
+public enum OrderStatus {
+    ORDER, CANCEL
+}
