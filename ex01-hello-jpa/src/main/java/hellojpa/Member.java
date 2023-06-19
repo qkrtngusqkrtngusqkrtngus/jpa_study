4645fd8a8631b879ff52b8493c2b1e13ba87ca53
hongchii <flyh0321@gmail.com>
2023-06-19 23:17:26 +0900

    다대다

--- ex01-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex01-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,6 +1,8 @@
 package hellojpa;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @SequenceGenerator(name = "member_seq_generator", sequenceName = "member_seq")
@@ -20,6 +22,13 @@ public class Member {
     @JoinColumn(name = "LOCKER_ID")
     private Locker locker;
 
+    // 다대다는 실무에서 X 불필요한 데이터가 들어올 수 있다. 단순히 연결만 하고 끝나지 않는다. >> OneToMany , MayToOne (연결테이블을 엔티티로 승격)
+//    @ManyToMany
+//    @JoinTable(name = "MEMBER_PRODUCT")
+//    private List<Product> products = new ArrayList<>();
+
+    @OneToMany(mappedBy = "member")
+    private List<MemberProduct> memberProducts = new ArrayList<>();
     public Long getId() {
         return id;
     }
---
+++ ex01-hello-jpa/src/main/java/hellojpa/Product.java
@@ -0,0 +1,36 @@
+package hellojpa;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Product {
+
+    @Id @GeneratedValue
+    private Long Id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "product")
+    private List<Member> memberProducts = new ArrayList<>();
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
